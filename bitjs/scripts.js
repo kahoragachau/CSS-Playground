ce8c9f1dd96cd101e2fbea797c1b20dc8faa563c
kahoragachau <joelkahoragachau6@gmail.com>
2022-09-14 18:34:42 +0300

    - use style within a function

--- bitjs/scripts.js
+++ bitjs/scripts.js
@@ -1,10 +1,10 @@
-// const body = document.querySelector("body")
-// const tracker = document.getElementById("tracker")
-// tracker.addEventListener("mouseover", ()=> {
-//     // tracker.style.backgroundColor = "green"
-//     tracker.style.width="75px";
-//     tracker.style.borderLeftColor="black"
-//     tracker.style.borderRightColor="yellow"
-//     tracker.style.borderTopColor="white"
-//     // tracker.style.borderColor="pink"
-// })
+const body = document.querySelector("body")
+const tracker = document.getElementById("tracker")
+tracker.addEventListener("mouseover", ()=> {
+    // tracker.style.backgroundColor = "green"
+    tracker.style.width="75px";
+    tracker.style.borderLeftColor="black"
+    tracker.style.borderRightColor="yellow"
+    tracker.style.borderTopColor="white"
+    // tracker.style.borderColor="pink"
+})
